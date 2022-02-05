3e56eb9f057dde154a9a31b0c5d3cb220855006f
vignesh03 <alphvignesh98@gmail.com>
2022-02-05 16:27:02 +0530

    feat(app) : Added new type for comment and like and added new mutations for post operations

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,10 +1,23 @@
 const { gql } = require("apollo-server");
 module.exports = gql`
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
+  }
   type Post {
     id: ID!
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
   }
   type User {
     id: ID!
@@ -13,16 +26,28 @@ module.exports = gql`
     username: String!
     createdAt: String!
   }
+
   input RegisterInput {
     username: String!
     password: String!
     confirmPassword: String!
     email: String!
   }
+  input LoginInput {
+    username: String!
+    password: String!
+  }
   type Query {
     getPosts: [Post]
+    getPost(id: ID!): Post
   }
   type Mutation {
-    register(registerInput: RegisterInput): User!
+    register(registerInput: RegisterInput!): User!
+    login(loginInput: LoginInput!): User!
+    createPost(body: String!): Post!
+    deletePost(id: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): String
+    likePost(postId: ID!): Post!
   }
 `;
