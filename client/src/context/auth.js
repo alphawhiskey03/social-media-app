214674139a26f865784ef682b2588347d8a3074e
vignesh03 <alphvignesh98@gmail.com>
2022-02-15 00:32:58 +0530

    feat(app): Added logic to persist token and check for expiration

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,20 +1,33 @@
 import React, { createContext, useReducer } from "react";
+import jwtDecode from "jwt-decode";
 
 const initialState = {
   user: null,
   login: (userData) => {},
   logout: () => {},
 };
+if (localStorage.getItem("jwtToken")) {
+  const decodedToken = jwtDecode(localStorage.getItem("jwtToken"));
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem("jwtToken");
+  } else {
+    console.log(decodedToken);
+    initialState.user = decodedToken;
+  }
+}
+
 const AuthContext = createContext(initialState);
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
+      localStorage.setItem("jwtToken", action.payload.token);
       return {
         ...state,
         user: action.payload,
       };
     case "LOGOUT":
+      localStorage.removeItem("jwtToken");
       return {
         ...state,
         user: null,
