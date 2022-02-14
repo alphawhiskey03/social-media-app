669f3881ec0ba4c28ca6a08975b6f9c66d58c103
vignesh03 <alphvignesh98@gmail.com>
2022-02-15 00:31:10 +0530

    feat(app): using setContext to add authorization headers

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "apollo-link-context";
+const authLink = setContext((req) => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    header: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+const httpLink = createHttpLink({
   uri: "http://localhost:3002/",
+});
+
+console.log(authLink.concat(httpLink));
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 ReactDOM.render(
