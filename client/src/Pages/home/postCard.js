409187514a51b738eec3bfb0656fa9181654e194
vignesh03 <alphvignesh98@gmail.com>
2022-02-20 15:44:39 +0530

    fix(app) : Added  Masonry container and styling fixes

--- client/src/Pages/home/postCard.js
+++ client/src/Pages/home/postCard.js
@@ -1,79 +1,110 @@
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Button, Tooltip } from "@mui/material";
 import moment from "moment";
-import FavoriteIcon from "@mui/icons-material/Favorite";
 import CommentIcon from "@mui/icons-material/Comment";
 import AddPost from "./addPost";
-import {
-  Grid,
-  CardHeader,
-  Avatar,
-  IconButton,
-  Typography,
-} from "@mui/material";
+import Zoom from "@mui/material/Zoom";
+import { makeStyles } from "@mui/styles";
+import { Link } from "react-router-dom";
+import { Grid, CardHeader, Avatar, Typography } from "@mui/material";
 import { AuthContext } from "../../context/auth";
 import { useContext } from "react";
-
+import LikeButton from "../../components/likeButton";
+import DeleteButton from "../../components/deleteButton";
+import { Colors } from "../../utils/theme";
+import Masonry from "@mui/lab/Masonry";
+const useStyles = makeStyles({
+  body: {
+    textDecoration: "none",
+    color: "inherit",
+    cursor: "pointer",
+  },
+  cardHeader: {
+    "& .MuiCardHeader-title": {
+      color: Colors.secondary,
+    },
+  },
+});
 const PostCard = ({ posts }) => {
   const { user } = useContext(AuthContext);
+  const classes = useStyles();
+  const columnSize = () => {
+    if (posts.length <= 2) {
+      return 2;
+    } else if (posts.length <= 3) {
+      return 3;
+    } else {
+      return 4;
+    }
+  };
   return (
-    <Grid container spacing={2}>
-      {user && (
-        <Grid item md={4} xs={12}>
-          <AddPost />
-        </Grid>
-      )}
-      {posts &&
-        posts.map((post) => (
-          <Grid item md={4} xs={12} key={post.body}>
-            <Card
-              sx={{ minWidth: 50 }}
-              style={{ backgroundColor: "#e8f4fd", borderRadius: 15 }}
-              elevation={5}
-            >
-              <CardHeader
-                avatar={
-                  <Avatar
-                    aria-label="recipe"
-                    style={{
-                      backgroundColor: "white",
-                      border: "1px solid grey",
-                    }}
-                  >
-                    <img
-                      src="./logo192.png"
-                      style={{ height: 30, width: 30 }}
-                    />
-                  </Avatar>
-                }
-                action={
-                  <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                  </IconButton>
-                }
-                title={post.username}
-                subheader={moment(post.createdAt).fromNow(true)}
-              />
-              <CardContent>
-                <div style={{ marginTop: 5 }}>
-                  <Typography>{post.body}</Typography>
-                </div>
-              </CardContent>
-              <CardActions>
-                <Button size="small" color="info" startIcon={<FavoriteIcon />}>
-                  {post.likeCount}
-                </Button>
-                <Button size="small" color="info" startIcon={<CommentIcon />}>
-                  {post.commentCount}
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-    </Grid>
+    <Zoom in={true} style={{ transitionDelay: "500ms" }}>
+      <Masonry columns={columnSize()} spacing={1}>
+        {user && (
+          <>
+            <AddPost />
+          </>
+        )}
+        {posts &&
+          posts.map((post) => (
+            <>
+              <Card
+                sx={{ minWidth: 50 }}
+                style={{ backgroundColor: Colors.primary, borderRadius: 15 }}
+                elevation={5}
+              >
+                <CardHeader
+                  avatar={
+                    <Avatar
+                      aria-label="recipe"
+                      style={{
+                        backgroundColor: "white",
+                        border: "1px solid " + Colors.text.primary,
+                      }}
+                    >
+                      <img
+                        src="https://i.pinimg.com/originals/7b/aa/25/7baa252dbdfeed669c152bedd2fa5feb.jpg"
+                        style={{ height: 50, width: 50 }}
+                      />
+                    </Avatar>
+                  }
+                  action={
+                    user &&
+                    user.username === post.username && (
+                      <DeleteButton postId={post.id} />
+                    )
+                  }
+                  title={post.username}
+                  subheader={moment(post.createdAt).fromNow(true)}
+                  className={classes.cardHeader}
+                />
+                <CardContent>
+                  <div style={{ marginTop: 5 }}>
+                    <Link to={`/post/${post.id}`} className={classes.body}>
+                      <Typography color="secondary">{post.body}</Typography>
+                    </Link>
+                  </div>
+                </CardContent>
+                <CardActions>
+                  <LikeButton post={post} user={user} />
+                  <Tooltip title={"comment"}>
+                    <Button
+                      size="small"
+                      href={`/post/${post.id}/comment`}
+                      color="secondary"
+                      startIcon={<CommentIcon />}
+                    >
+                      {post.commentCount}
+                    </Button>
+                  </Tooltip>
+                </CardActions>
+              </Card>
+            </>
+          ))}
+      </Masonry>
+    </Zoom>
   );
 };
 
