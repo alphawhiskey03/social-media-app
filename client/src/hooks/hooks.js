e62d64c73aaf5dba489cda4f405531085bae8de3
vignesh03 <alphvignesh98@gmail.com>
2022-02-20 15:39:10 +0530

    feat(app): Added validation for input values

--- client/src/hooks/hooks.js
+++ client/src/hooks/hooks.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
 export const useForm = (callback, initialState = {}) => {
-  const [values, setValues] = useState();
+  const [values, setValues] = useState(initialState);
   const onChange = (event) => {
     setValues({ ...values, [event.target.name]: event.target.value });
   };
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(values);
-    callback();
+    const isValidated = Object.values(values).every((value) => value !== "");
+    if (isValidated) {
+      callback();
+    }
   };
 
   return {
