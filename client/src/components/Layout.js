52a667a55eee62029d276e3defd3bee9d9cef596
vignesh03 <alphvignesh98@gmail.com>
2022-03-23 23:29:58 +0530

    feat(app): Included footer to the layout

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,13 +1,15 @@
 import { Container, Box } from "@mui/material";
 import MenuBar from "./MenuBar";
+import Footer from "./footer";
 
 const Layout = ({ children }) => {
   return (
     <>
       <MenuBar />
-      <Container>
+      <Container style={{ minHeight: "75vh" }}>
         <Box style={{ marginTop: 20 }}>{children}</Box>
       </Container>
+      <Footer />
     </>
   );
 };
